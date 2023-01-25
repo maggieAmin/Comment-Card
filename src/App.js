af7098745f10102158901e2542b459a270ee9936
Maggie Amin <maggiemmagdy@gmail>
2023-01-25 14:41:30 +0000

    just started working on it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import CommentCard from './components/CommentCard';
 
 function App() {
+
+  const [comments, setComments] = useState([
+    {
+      imageUrl: "https://picsum.photos/100",
+      userName: "William Wise",
+      datePosted: new Date(2022, 7, 1),
+      comment: "This comment is the wisest thing you've ever read"
+    },
+    {
+      imageUrl: "https://picsum.photos/100/100",
+      userName: "Buddha",
+      datePosted: new Date(600, 6, 18),
+      comment: "As your focus transcends all types of objects, Unfixed on any mark of concreteness, Remain quiet, tranquil and awake!"
+    }
+  ]);
+
+  const cards = comments.map( (commentObj, i) => {
+    return(
+      <CommentCard key={i}
+        imageUrl={commentObj.imageUrl}
+        userName={commentObj.userName}
+        datePosted={commentObj.datePosted} >
+          {commentObj.comment}
+      </CommentCard>
+    )
+  } );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>{cards}</ul>
+  )
+
 }
 
 export default App;
+
---
+++ src/components/CommentCard.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+const Ul = styled.ul`
+
+`
+const CommentCard = ({cards}) => {
+    return (
+        <Ul>
+            {cards.imageUrl}
+            
+        </Ul>
+
+    )
+}
+
+
+
+
+
+
+export default CommentCard
